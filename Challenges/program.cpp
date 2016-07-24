9cc1a3ce485ff44533ed8dbe2981559612ddb7b6
Avi Zadok <Avizadok@gmail.com>
2016-07-24 20:27:18 +0300

    Initial commit

---
+++ Challenges/program.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main() {
+
+	string str;
+
+	cout << "Enter line" << endl;
+
+	cin >> str;
+
+
+	cout << "You entered: " << str;
+
+	return 0;
+
+
+}
